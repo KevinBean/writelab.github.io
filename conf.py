d6cf1a44af068203a4ec4efd011653454b864acc
KevinBean <bianbin@bianbindeiMac.local>
2017-07-15 23:26:41 +0800

    Nikola auto commit.
    Nikola version: 7.8.9

--- conf.py
+++ conf.py
@@ -141,7 +141,15 @@ NAVIGATION_LINKS = {
 }
 
 # Name of the theme to use.
-THEME = "bootstrap3"
+# THEME = "bootstrap3"
+
+THEME = 'canterville'
+LOGO_URL = 'https://getnikola.com/assets/img/logo.svg'
+GLOBAL_CONTEXT = {
+         'TWITTER_URL': 'https://twitter.com/kevinbean',
+         'GITHUB_URL': 'https://github.com/kevinbean',
+         'BANNER_URL': '/assets/img/silk-road.jpg'
+}
 
 # Primary color of your theme. This will be used to customize your theme and
 # auto-generate related colors in POSTS_SECTION_COLORS. Must be a HEX value.
